0f2f2ca3769edf63f053f0ac2561c16a6709333c
Raúl Sepúlveda <raulsetron@gmail.com>
2018-01-14 11:37:16 -0300

    inicio proyecto, un arreglo con artista a mano, for en li

---
+++ lastmusic/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h2>LastFm Music</h2>
+    <ul>
+      <li v-for="artist in artists">{{artist.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [{name:'Juanes'},{name:'Eddie Vedder'},{name:'John Lennon'}]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
