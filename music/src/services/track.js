d24c1f44e5285412819acd43e8ea6316fbe5fda3
Raúl Sepúlveda <raulsetron@gmail.com>
2018-01-16 19:34:17 -0300

    Fetch API & Trae
    acá solo configuramos como ocupar trae y llamar alguna api (ejemplo
    pero aún no aplica con url)

---
+++ music/src/services/track.js
@@ -0,0 +1,16 @@
+import musicService from './music'
+
+const = trackService = {}
+
+trackService.search = function (q) {
+  const type = 'track'
+
+  return musicService.get('/search', {
+    params: { q: q, type: type }
+  })
+  .then(function(res) {
+    return res.data
+  })
+}
+
+export default trackService
