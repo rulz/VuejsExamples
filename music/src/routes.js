a62f094e0eaade8f47e54808693e33abdcc04f48
Raúl Sepúlveda <raulsetron@gmail.com>
2018-01-17 23:42:10 -0300

    casi Crear y Navegar Rutas con router-link

--- music/src/routes.js
+++ music/src/routes.js
@@ -1,7 +1,10 @@
 import Search from '@/components/Search.vue'
+import About from '@/components/About.vue'
 
 const routes = [
-  { path: '/', component: Search, name: 'search' }
+  { path: '/', component: Search, name: 'search' },
+  { path: '/about', component: About, name: 'about' }
+  //{ path: '/track/:id', component: About, name: 'about' }
 ]
 
 export default routes
